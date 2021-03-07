4fabc38bc204255765e92d67d727b6edad78f2b1
Shaul Fridman <shaul.fridman@gmail.com>
2021-03-07 09:10:53 +0200

    WIP

--- recipes/antlr4/all/conanfile.py
+++ recipes/antlr4/all/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class Antlr4Conan(ConanFile):
     name = "antlr4"
-    license = "The BSD License"
+    license = "BSD-3-Clause"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://www.antlr.org/"
     topics = ("conan", "antlr", "parsers")
@@ -16,6 +16,8 @@ class Antlr4Conan(ConanFile):
     generators = "cmake"
     requires = "libuuid/1.0.3"
 
+    _cmake = None
+
     @property
     def _source_subfolder(self):
         return "source_subfolder"
@@ -24,37 +26,37 @@ class Antlr4Conan(ConanFile):
     def _build_subfolder(self):
         return "build_subfolder"
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
     def source(self):
-        tools.get(
-            **self.conan_data["sources"][self.version],
-            destination=self._source_subfolder
-        )
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder)
+        tools.get(**self.conan_data["sources"][f"{self.version}-antlr.jar"],
+                  destination=self._source_subfolder)
 
     def build(self):
         for patch in self.conan_data["patches"][self.version]:
-            print("Looking for {}".format(patch))
             tools.patch(**patch)
         cmake = self._configure_cmake()
-        if self.options.shared:
-            cmake.build(target="antlr4_shared")
-        else:
-            cmake.build(target="antlr4_static")
+        cmake.build(
+            target="antlr4_shared" if self.options.shared else "antlr4_static")
 
     def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["WITH_LIBCXX"] = "OFF"
-        cmake.configure(
-            build_folder=self._build_subfolder, source_folder=self._source_subfolder
-        )
-        return cmake
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["WITH_LIBCXX"] = "OFF"
+        self._cmake.configure(build_folder=self._build_subfolder,
+                              source_folder=self._source_subfolder)
+        return self._cmake
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
-        self.copy(
-            "*.h",
-            dst="include",
-            src=os.path.join(self._source_subfolder, "runtime", "src"),
-        )
+        self.copy(pattern="LICENSE.txt", dst="licenses",
+                  src=self._source_subfolder)
+        self.copy("*.h", dst="include",
+                  src=os.path.join(self._source_subfolder, "runtime", "src"))
         self.copy("*.a", dst="lib", keep_path=False)
         self.copy("*.jar", dst="bin", keep_path=False)
         self.copy(
