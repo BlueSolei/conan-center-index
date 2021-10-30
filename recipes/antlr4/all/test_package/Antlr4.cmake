4a74d932a4f736bfe18599995c576ec5121af3d2
Shaul Fridman <shaul.fridman@gmail.com>
2021-10-30 04:09:30 +0300

    (~) dependency on generated files is done in cmake's build phase

--- recipes/antlr4/all/test_package/Antlr4.cmake
+++ recipes/antlr4/all/test_package/Antlr4.cmake
@@ -1,7 +1,6 @@
 cmake_minimum_required(VERSION 3.19)
 
-set(CMAKE_CXX_STANDARD 11)
-set(CMAKE_CXX_EXTENSIONS OFF)
+include(CMakePrintHelpers)
 
 find_package(
   Java
@@ -12,33 +11,42 @@ if(NOT UNIX)
   message(WARNING "Unsupported operating system")
 endif()
 
+# generate parser for taget
 function(antlr4_generate_parser target)
   cmake_parse_arguments(PARSE_ARGV 1 ARG "" "NAME"
                         "GRAMMER_FILES;ANTLR_JAR_ARGS")
-  set(GENERATED_SOURCES_DIR ${CMAKE_CURRENT_BINARY_DIR})
-  set(GENERATED_SOURCES_VAR "${target}-${NAME}_GENERATED_SRC")
-  set(GRAMMER_TARGET "${target}-${NAME}")
 
-  # Make the generated files depend on the grammer files
-  set(build_input_files) # Will be list of files copied into build tree
-  foreach(input_file ${ARG_GRAMMER_FILES})
-    # Extract name of the file for generate path of the file in the build tree
-    get_filename_component(input_file_name ${input_file} NAME)
-    # Path to the file created by copy
-    set(build_input_file ${GENERATED_SOURCES_DIR}/${input_file_name})
-    # Copy file
-    configure_file(${input_file} ${build_input_file} COPY_ONLY)
-    # Add name of created file into the list
-    list(APPEND build_input_files ${build_input_file})
-  endforeach()
-
-  # generate parser
   if(NOT ARG_ANTLR_JAR_ARGS)
     set(ARG_ANTLR_JAR_ARGS -listener -visitor)
   endif()
+  set(GENERATED_SOURCES_DIR ${CMAKE_CURRENT_BINARY_DIR})
+  set(GRAMMER_TARGET "${target}-${NAME}")
 
-  message(STATUS "Antlr4: generating parser for grammer '${ARG_NAME}'")
-  set(INVOKE_ANTLR_CMD
+  # Antlr4 requires C++11 or newer stansrad
+  get_target_property(CPP_STD ${target} CXX_STANDARD)
+  if(NOT CPP_STD)
+    message(
+      WARNING
+        "Antlr4: antlr's parsers require at least C++11. seting C++11 standard for target '${target}'"
+    )
+    set_target_properties(
+      ${target}
+      PROPERTIES CXX_STANDARD 11
+                 CXX_STANDARD_REQUIRED YES
+                 CXX_EXTENSIONS NO)
+  endif()
+
+  # ~~~
+  # Make the generated files depend on the grammer files
+  # PROBLEM: CMake is difficult in depending on generated files which are
+  #   unknown in configure time. the antlr4 generated files (names and count) depend on the
+  #   parameters & the files it is invoked with.
+  # SOLUTION: generate once, so we know exactly which files are generated.
+  #   then, we depend on these files with add_custom_target()
+  # ~~~
+  message(
+    STATUS "Antlr4: generating parser for grammer '${ARG_NAME}' - first time")
+  set(GENERATE_PARSER_CMD
       "${Java_JAVA_EXECUTABLE}"
       -jar
       ${CONAN_USER_ANTLR4_antlr_jar}
@@ -48,43 +56,27 @@ function(antlr4_generate_parser target)
       ${ARG_ANTLR_JAR_ARGS}
       -o
       ${GENERATED_SOURCES_DIR}
-      ${build_input_files})
+      ${ARG_GRAMMER_FILES})
 
+  # generate once to collect generated file names
   execute_process(
-    COMMAND ${INVOKE_ANTLR_CMD} WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
-                                                  COMMAND_ERROR_IS_FATAL ANY)
+    COMMAND ${GENERATE_PARSER_CMD} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                                                     COMMAND_ERROR_IS_FATAL ANY)
+  file(GLOB GENERATED_SOURCES ${GENERATED_SOURCES_DIR}/*.cpp)
+  message(
+    STATUS
+      "Antlr4: generating parser for grammer '${ARG_NAME}' - first time - done")
 
-  file(GLOB ${GENERATED_SOURCES_VAR} "${GENERATED_SOURCES_DIR}/*.cpp")
-  target_sources(${target} PRIVATE ${${GENERATED_SOURCES_VAR}})
-
-  add_custom_target(${GRAMMER_TARGET} DEPENDS ${${ARG_GRAMMER_FILES}})
+  # generate the parser (on build step) when grammer files changes
+  add_custom_command(
+    OUTPUT ${GENERATED_SOURCES}
+    COMMAND ${GENERATE_PARSER_CMD}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    DEPENDS ${ARG_GRAMMER_FILES}
+    COMMENT "Antlr4: generating parser for grammer '${ARG_NAME}'")
+  add_custom_target(${GRAMMER_TARGET} DEPENDS ${ARG_GRAMMER_FILES})
   add_dependencies(${target} ${GRAMMER_TARGET})
+
+  target_sources(${target} PRIVATE ${GENERATED_SOURCES})
   target_include_directories(${target} PRIVATE ${GENERATED_SOURCES_DIR})
 endfunction()
-
-# set(${GENERATED_SOURCES_VAR} ${GENERATED_SOURCES_DIR}/TLexer.cpp
-# ${GENERATED_SOURCES_DIR}/TParser.cpp
-# ${GENERATED_SOURCES_DIR}/TParserBaseListener.cpp
-# ${GENERATED_SOURCES_DIR}/TParserBaseVisitor.cpp
-# ${GENERATED_SOURCES_DIR}/TParserListener.cpp
-# ${GENERATED_SOURCES_DIR}/TParserVisitor.cpp)
-
-# foreach(src_file ${${GENERATED_SOURCES_VAR}})
-# set_source_files_properties(${src_file} PROPERTIES GENERATED TRUE)
-# endforeach(src_file ${${GENERATED_SOURCES_VAR}})
-
-# add_custom_command( OUTPUT ${${GENERATED_SOURCES_VAR}} COMMAND
-# ${CMAKE_COMMAND} -E make_directory ${GENERATED_SOURCES_DIR}/ COMMAND
-# "${Java_JAVA_EXECUTABLE}" -jar ${ANTLR_JAR_LOCATION} -Werror -Dlanguage=Cpp
-# -listener -visitor -o ${GENERATED_SOURCES_DIR}/ -package antlrcpptest
-# ${PROJECT_SOURCE_DIR}/TLexer.g4 ${PROJECT_SOURCE_DIR}/TParser.g4
-# WORKING_DIRECTORY "${CMAKE_BINARY_DIR}" DEPENDS
-# ${PROJECT_SOURCE_DIR}/TLexer.g4 ${PROJECT_SOURCE_DIR}/TParser.g4 COMMENT
-# "antlr4 code generation (lexer + parser)")
-
-# if(NOT CMAKE_CXX_COMPILER_ID MATCHES "MSVC") set(flags_1
-# "-Wno-overloaded-virtual") else() set(flags_1 "-MP /wd4251") endif()
-
-# foreach(src_file ${antlr4-demo_SRC}) set_source_files_properties( ${src_file}
-# PROPERTIES COMPILE_FLAGS "${COMPILE_FLAGS} ${flags_1}") endforeach(src_file
-# ${antlr4-demo_SRC})
